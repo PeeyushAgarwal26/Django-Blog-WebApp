a586ee70b7e809103d397b1bd300727c6f1ea7f3
PeeyushAgarwal26 <peeyush.agarwal01@gmail.com>
2025-05-07 15:15:15 +0530

    Final update for hosting

--- BlogWebsite/settings.py
+++ BlogWebsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*+k7y9g)#u$)k^g=%3^gpg&u$76uqmbkuil&y!*-qe(#4ufyp3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), BASE_DIR/'static'] # ADDED MANUALLY
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # ADDED MANUALLY
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static') # ADDED MANUALLY
 
 # Default primary key field type
