8511c1c1607555193a01bc7307b7ae650aa7e5d9
PeeyushAgarwal26 <peeyush.agarwal01@gmail.com>
2025-05-07 13:33:04 +0530

    Addition of Config Settings for Hosting

--- BlogWebsite/urls.py
+++ BlogWebsite/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 from django.contrib.auth.urls import views as auth_views
 
 urlpatterns = [
+    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
